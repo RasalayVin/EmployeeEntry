5de6a01f90678d4791c7ac5938053ba34655027d
RasalayVin <rasalay.vin@gmail.com>
2020-12-19 22:24:48 +0530

    well formed

--- EmployeeEntrySystem/src/main/java/com/ees/entity/EmployeeDetails.java
+++ EmployeeEntrySystem/src/main/java/com/ees/entity/EmployeeDetails.java
@@ -2,49 +2,64 @@ package com.ees.entity;
 
 import java.io.Serializable;
 
-public class EmployeeDetails implements Serializable{
+public class EmployeeDetails implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	protected int employeeNo;
 	protected String employeeName;
 	protected double salaray;
 	protected String address;
+
 	public int getEmployeeNo() {
 		return employeeNo;
 	}
+
 	public void setEmployeeNo(int employeeNo) {
 		this.employeeNo = employeeNo;
 	}
+
 	public String getEmployeeName() {
 		return employeeName;
 	}
+
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
+
 	public double getSalaray() {
 		return salaray;
 	}
+
 	public void setSalaray(double salaray) {
 		this.salaray = salaray;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
 	public EmployeeDetails() {
 	}
+
 	public EmployeeDetails(int employeeNo, String employeeName, double salaray, String address) {
 		super();
 		this.employeeNo = employeeNo;
 		this.employeeName = employeeName;
 		this.salaray = salaray;
 		this.address = address;
+
 	}
+
 	@Override
 	public String toString() {
 		return "EmployeeDetails [employeeNo=" + employeeNo + ", employeeName=" + employeeName + ", salaray=" + salaray
 				+ ", address=" + address + "]";
 	}
-	
+
 }
